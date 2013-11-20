65fb377bdc4760cde0244b78e7fcc3b761bd209e
Benoît Suttor <bsuttor@cirb.irisnet.be>
2013-11-20 15:59:53 +0100

    add js and css files

---
+++ src/prb/arsene50/browser/static/arsene50.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+     $("div.title[rel]").overlay();
+     $( "#accordion" ).accordion({
+         heightStyle: "content",
+         collapsible: true,
+         disable: true,
+     });
+
+ });
