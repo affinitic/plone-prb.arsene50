992ea1b01ac670216f13de0574fe8ddf495e5281
Laurent Lasudry <lasudry@gmail.com>
2020-02-12 16:59:34 +0100

    Improve styles & JS
    This refs cirb #262

--- src/prb/arsene50/browser/static/arsene50.js
+++ src/prb/arsene50/browser/static/arsene50.js
@@ -1,15 +1,2 @@
 $(document).ready(function() {
-    $("#accordion2").accordion({
-        collapsible: true,
-        header: "div.accordion-heading",
-        heightStyle: "content",
-    });
-    $(".moreinfo").hide();
-    $(".show_hide").show();
-
-    $('a.show_hide').click(function(event){
-        rel = event.currentTarget.rel;
-        $(rel).slideToggle();
-        event.preventDefault();
-    });
 });
